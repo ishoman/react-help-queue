8a5c6207021d5070ae5ffceb1e9d6dc20b02e930
Isaac Shoman <isaac.shoman@gmail.com>
2018-06-21 13:41:48 -0700

    Start live server lesson

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    resolve(__dirname, "src") + "/index.jsx"
+  ],
+
+  output: {
+    filename: 'app.bundle.js',
+    path: resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "es2015",
+            "react"
+          ]
+        }
+      },
+    ],
+  }
+};
