06d33f6ec7e921e7d9ae8c2b7903aa9eb54f62d3
Isaac Shoman <isaac.shoman@gmail.com>
2018-06-25 20:32:13 -0700

    Add linter, jsx css modules

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
     <div>
-      <h3>{props.location} - {props.name}</h3>
+      <style jsx>{`
+        div {
+          background-color: red;
+        }
+      `}</style>
+      <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
